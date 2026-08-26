0089173de743c1eb45645a8e81a4c1f5c5a846b9
192524461simats <192524461.simats@saveetha.com>
2026-08-26 23:00:08 +0530

    Add files via upload

---
+++ EXP6/EXP6.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+int main()
+{
+    char id[50];
+    int i, valid = 1;
+
+    printf("Enter identifier: ");
+    scanf("%s", id);
+
+    if (!isalpha(id[0]) && id[0] != '_')
+        valid = 0;
+
+    for (i = 1; id[i] != '\0'; i++)
+    {
+        if (!isalnum(id[i]) && id[i] != '_')
+        {
+            valid = 0;
+            break;
+        }
+    }
+
+    if (valid)
